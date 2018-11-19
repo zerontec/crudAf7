4705eb0aea7e106bfc18d6963f4bb664faaaede5
unknown <zerontecnologia1@gmail.com>
2018-11-19 09:58:29 -0400

    generacion de componentes FrontEnd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './componets/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { HomeComponent } from './components/home/home.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { OffersComponent } from './components/offers/offers.component';
+import { Page404Component } from './components/page404/page404.component';
+import { DetailsBookComponent } from './components/details-book/details-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    ProfileComponent,
+    HeroComponent,
+    HomeComponent,
+    ModalComponent,
+    NavbarComponent,
+    OffersComponent,
+    Page404Component,
+    DetailsBookComponent
   ],
   imports: [
     BrowserModule
